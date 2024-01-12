538eefb17397c743858d02177e276753b32c46bb
benji <you@example.com>
2024-01-12 09:04:43 -0800

    .

--- term.c
+++ term.c
@@ -1,20 +1,51 @@
-// code copied from [https://www.uninformativ.de/git/eduterm/commit/94cbafa06db99c5deb7d13b046b06d2604a454a1.html]
-
-#define _XOPEN_SOURCE 600
-#include <ctype.h>
-#include <fcntl.h>
-#include <stdbool.h>
+// remaking bc idk how code works
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <string.h>
 #include <unistd.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include<signal.h>
 
-/* Font selection has not been implemented*/
+#define MAX_COMMAND_LENGTH 1024
 
-#define FONT_W 8
-#define FONT_H 16
-#define FONT_ASC 13
+void execute_command(const char *command) {
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("Fork failed");
+    } else if (pid == 0) {
+        // child process
+        execlp(command, command, NULL);
+        perror("Exec failed");
+        exit(1);
+    } else {
+        // parent process
+        wait(NULL);
+    }
+}
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        printf("Enter a command (or type 'exit' to quit): ");
+        fgets(command, sizeof(command), stdin);
+
+        //remove newline character
+        size_t length = strlen(command);
+        if (length > 0 && command[length - 1] == '\n') {
+            command[length - 1] = '\0';
+        }
+
+        // exit loop if user enters 'exit'
+        if (strcmp(command, "exit") == 0){
+            break;
+        }
+
+        // execute entered command
+        execute_command(command);
+    }
+
+    return 0;
+}
